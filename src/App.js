6cd98bd005af8d5791e98b341d87cbdeb0d5fcb4
Robert Alexander Iatan <robertiatan@gmail.com>
2023-03-24 01:21:44 -0400

    deployment test 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import "./App.css";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Portfolio from "./pages/Portfolio";
@@ -8,7 +9,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
 import { Route, Routes } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
       <Navbar />
       <div className="container">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/react-portfolio" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
